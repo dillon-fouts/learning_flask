d824ff11215c1a108b01f82977608474ae04b602
Dillon Fouts <dillon-fouts@uiowa.edu>
2024-03-27 16:43:13 -0500

    dillon app

--- app.py
+++ app.py
@@ -13,5 +13,12 @@ def tyler():
     sum = n1 + n2
     return str(sum)
 
+@app.route("/dillon")
+def dillon():
+    n1=11
+    n2=20
+    sum = n1 + n2
+    return str(sum)
+
 if __name__ == "__main__":
     app.run(debug=True)
