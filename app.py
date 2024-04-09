1b49f6d36ef8b96feeb2e8c1170a472c2e3129aa
Tyler Noga <tyler-noga@uiowa.edu>
2024-04-08 20:03:39 -0500

    Updated new form, fixed app.py

--- app.py
+++ app.py
@@ -2,52 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-books = []
+experience = []
 
 # Define routes
 @app.route("/")
 def index():
-    return render_template("index.html", books=books)
+    return render_template("index.html", experience=experience)
 
 @app.route("/add", methods=["POST"])
 def add():
-    title = request.form.get("title")
-    author = request.form.get("author")
-    pages = request.form.get("pages")
-    classification = request.form.get("classification")
-    details = request.form.getlist("details")
-    how = request.form.get("how")
-    
-    # Modify classification to include hyphen
-    if classification == "nonfiction":
-        classification = "non-fiction"
-    
-    # Modify details to display shorter versions
-    details_mapping = {
-        "I own this book": "own",
-        "I've read this book": "read",
-        "I liked this book": "liked",
-        "I would recommend this book to others": "recommend"
-    }
-    details_short = ", ".join([details_mapping.get(detail, detail) for detail in details])
-    
-    # Modify acquisition to display shorter versions
-    acquisition_mapping = {
-        "I purchased it": "purchased",
-        "It was a gift": "gift",
-        "I checked it out from the library": "library",
-        "A friend loaned it to me": "friend"
-    }
-    how_short = acquisition_mapping.get(how, how)
-    
-    books.append({
+    age = request.form.get("age")
+    gender = request.form.get("gender")
+    country = request.form.get("country")
+    degree = request.form.get("highest_deg")
+    code_experience = request.form.getlist("code_experience")
+    title = request.form.get("current_title")
+    company_size = request.form.get("company_size")
+      
+    experience.append({
+        "age": age,
+        "gender": gender,
+        "country": country,
+        "degree": degree,
+        "code_experience": code_experience,
         "title": title,
-        "author": author,
-        "pages": pages,
-        "classification": classification,
-        "details": details_short,
-        "how": how_short
+        "size": company_size
     })
+    
     return redirect(url_for("index"))
 
 @app.route("/about")
